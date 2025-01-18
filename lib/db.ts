dc019bb3bde09801d46af41d79fc713a7a2ca5a7
nArsenych <narsenych@gmail.com>
2025-01-18 23:16:58 +0200

    minor changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
-  interface Global {
-    prisma: PrismaClient | undefined;
-  }
+  var prisma: PrismaClient | undefined;
 }
 
-const globalForPrisma = global as { prisma?: PrismaClient };
-
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
+  global.prisma = db;
 }
